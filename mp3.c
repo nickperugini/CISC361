a222eb65eab8a7376cf12eed4f6dbd2d483340d0
Nick Perugini <nickperugini10@gmail.com>
2021-09-17 15:23:33 -0400

    Update mp3.c

--- mp3.c
+++ mp3.c
@@ -5,100 +5,185 @@
 
 #define BUFFERSIZE 128
 
-struct mp3 {
-        char *name;
-        char *artist;
-        int year;
-        int time;
-        struct mp3 *next;
-        struct mp3 *prev;
-} *mp3_h,*tmp1,*tmp2,*tmp3;
+mp3_t *head;
 
-int main (int argc, char *argv[]){
+int main(int argc, char *argv[])
+{
 
-        mp3_h = NULL;
-        int n;
+	int n;
 
-        printf("To add a song type 1 \nTo delete a song type 2\nTo print list type 3\nTo print list inversely type 4\nTo exit type 5\n");
-        scanf("%d", &n);
+	while (1)
+	{
 
-        while(1){
+		printf("To add a song type 1 \nTo delete a song type 2\nTo print list type 3\nTo print list inversely type 4\nTo exit type 5\n");
+		scanf("%d", &n);
 
-                printf("To add a song type 1 \nTo delete a song type 2\nTo print list type 3\nTo print list inversely type 4\nTo exit type 5\n");
-                scanf("%d", &n);
+		switch (n)
+		{
 
-                if(n==1){
-                        addSong();
-                }
-                else if(n==2){
-                        removeSong();
-                }
-                else if(n==3){
-                        printDLL();
-                }
-                else if(n==4){
-                        printInverseDLL();
-                }
-                else if(n==5){
-                        printf("Exiting Program");
-                        return 0;
-                }
-                else{
-                        printf("Invalid input. Try Again");
-                }
-        }
+		case 1:
+			addSong();
+			break;
+		case 2:
+			removeSong();
+			break;
+		case 3:
+			printDLL();
+			break;
+		case 4:
+			printInverseDLL();
+			break;
+		case 5:
+			printf("Exiting Program");
+			exit(0);
+		default:
+			printf("Invalid input. Try Again: ");
+		}
+	}
 }
-void addSong(){
-        int len;
-        char  buffer[BUFFERSIZE];
+mp3_t *create()
+{
+	int nameLen;
+	int artistLen;
+	char name[BUFFERSIZE];
+	char artist[BUFFERSIZE];
+	fflush(stdin); //issues with scanf before fgets causing song name input to be skipped(or misread)
 
-        if(mp3_h==NULL){
-                tmp1 = (struct *mp3)malloc(sizeof(struct mp3));
+	mp3_t *tmp1 = (struct mp3 *)malloc(sizeof(struct mp3));
+	tmp1->prev = NULL;
+	tmp1->next = NULL;
 
-                printf("Enter song name: ");
-                if (fgets(buffer, BUFFERSIZE , stdin) != NULL){
-                len = (int) strlen(buffer);
-                buffer[len-1]='\0';
-                tmp1->name = (char *)malloc(len);
-                strcpy(tmp1->name, buffer);
-                }
-                printf("Enter artist name: ");
-                if (fgets(buffer, BUFFERSIZE , stdin) != NULL){
-                len = (int) strlen(buffer);
-                buffer[len-1]='\0';
-                tmp1->name = (char *)malloc(len);
-                strcpy(tmp1->artist, buffer);
-                }
-                printf("Enter year made: ");
-                scanf("%d", &(tmp1->year));
+	printf("Enter song name: ");
+	if (fgets(name, BUFFERSIZE, stdin) != NULL)
+	{
+		nameLen = (int)strlen(name);
+		name[nameLen - 1] = '\0';
+		tmp1->name = (char *)malloc(nameLen);
+		strcpy(tmp1->name, name);
+	}
 
-                printf("Enter song time: ");
-                scanf("%d", &(tmp1->time));
+	printf("Enter artist name: ");
+	if (fgets(artist, BUFFERSIZE, stdin) != NULL)
+	{
+		artistLen = (int)strlen(artist);
+		artist[artistLen - 1] = '\0';
+		tmp1->artist = (char *)malloc(artistLen);
+		strcpy(tmp1->artist, artist);
+	}
 
-                mp3_h=tmp1;
+	printf("Enter year made: ");
+	scanf("%d", &(tmp1->year));
 
-                tmp1->prev=NULL;
-                tmp1->next=NULL;
-        }
+	printf("Enter song time: ");
+	scanf("%d", &(tmp1->time));
+
+	return tmp1;
 }
 
-void removeSong(){printf("2");}
-
-void printDLL(){
-        tmp1=mp3_h;
-        while (tmp1!=NULL){
-                printf("Name: [%s] \n",tmp1->name);
-                tmp1=tmp1->next;
-        }
+void addSong()
+{
+	mp3_t *newNode = create();
+	if (head == NULL)
+	{
+		head = newNode;
+	}
+	else
+	{
+		mp3_t *tmp = head;
+		while (tmp->next != NULL)
+		{
+			tmp = tmp->next;
+		}
+		tmp->next = newNode;
+		newNode->prev = tmp;
+	}
 }
-void printInverseDLL(){
-        tmp1=mp3_h;
-        while (tmp1 != NULL) {
-                tmp2 = node;
-                tmp1 = tmp1->next;
-        }i
-        while (tmp2 != NULL){
-                printf(printf("Name: [%s] \n",tmp2->name);
-                tmp2=tmp2->prev;
-        }
+
+void removeSong()
+{
+	char toDel[BUFFERSIZE];
+	int toDelLen;
+	fflush(stdin);
+
+	printf("Enter artist of song(s) you want to delete: ");
+	if (fgets(toDel, BUFFERSIZE, stdin) != NULL)
+	{
+		toDelLen = (int)strlen(toDel);
+		toDel[toDelLen - 1] = '\0';
+		mp3_t *tmp1 = head;
+		
+		while (tmp1 != NULL)
+		{
+			if(strcmp(tmp1->artist, toDel)){
+			
+			if(tmp1->prev==NULL && tmp1->next==NULL){//if prev and next null (first and only song)
+				head=NULL;
+			}
+			else if(tmp1->prev==NULL && tmp1->next!=NULL){//if prev null and next not null (first song)
+				head=tmp1->next;
+				tmp1->next->prev=NULL;
+			}
+			else if(tmp1->prev!=NULL && tmp1->next!=NULL){//if prev and next not null (middle song)
+				tmp1->prev->next=tmp1->next;
+				tmp1->next->prev=tmp1->prev;
+			}
+			else if(tmp1->prev!=NULL && tmp1->next==NULL){//if prev not null and next null (end song)
+				tmp1->prev->next=NULL;
+			}
+			printf("Dleteing: [%s] \n", tmp1->name);
+			/*free(tmp1->name);
+			free(tmp1->artist);
+			free(tmp1);*/
+			tmp1=tmp1->next;
+			printf("\n-----------\n");
+			}
+		}
+	}
+}
+
+void printDLL()
+{
+	if (head == NULL)
+	{
+		printf("Nothing to print \n");
+	}
+	else
+	{
+		mp3_t *tmp1 = head;
+		while (tmp1 != NULL)
+		{
+			printf("Name: [%s] \n", tmp1->name);
+			printf("Artist: [%s] \n", tmp1->artist);
+			printf("Length: [%d] \n", tmp1->time);
+			printf("Year: [%d] \n", tmp1->year);
+			printf("-----------\n");
+			tmp1 = tmp1->next;
+		}
+	}
+}
+void printInverseDLL()
+{
+	if (head == NULL)
+	{
+		printf("Nothing to print \n");
+	}
+	else
+	{
+		mp3_t *tmp1 = head;
+		mp3_t *tmp2;
+		while (tmp1 != NULL)
+		{
+			tmp2 = tmp1;
+			tmp1 = tmp1->next;
+		}
+		while (tmp2 != NULL)
+		{
+			printf("Name: [%s] \n", tmp2->name);
+			printf("Artist: [%s] \n", tmp2->artist);
+			printf("Length: [%d] \n", tmp2->time);
+			printf("Year: [%d] \n", tmp2->year);
+			printf("-----------\n");
+			tmp2 = tmp2->prev;
+		}
+	}
 }
