7424c569cb87b08ec6744c7699f738fbd90757d4
Nick Perugini <nickperugini10@gmail.com>
2021-09-17 15:23:47 -0400

    Update mp3.h

--- mp3.h
+++ mp3.h
@@ -1,4 +1,13 @@
-#include <stdio.h>
+typedef struct mp3 {
+	char *name;
+	char *artist;
+	int year;
+	int time;
+	struct mp3 *next;
+	struct mp3 *prev;
+}mp3_t;
+
+mp3_t* create();
 void addSong();
 void removeSong();
 void printDLL();
